68c97f27c75b5ad7abe50d309353a7c91fcfce9a
Carlos de Moura <lcbarrosdemoura@gmail.com>
2026-04-14 15:47:06 -0300

    no more pre render

--- prerender.R
+++
@@ -1,7 +0,0 @@
-atualizar =
-  ( file.info("posts")$mtime > as.POSIXct(readLines("timestamp.txt")) ) |>
-  {\(.) ifelse(is.na(.), T, .)}()
-
-if (atualizar) {
-  cat(as.character(Sys.time()), "\n", file = "timestamp.txt")
-}
